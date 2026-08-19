f27020d65d0c5a805cc020f69605b92830b55b91
yadekola <yakubadekolaojo059@gmail.com>
2026-08-19 12:47:10 +0000

    first commit another vms

---
+++ firstscript.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+### This script prints system info ###
+
+echo "Welcome to bash script."
+echo
+
+echo "###########################"
+
+#Checking system uptime#
+echo "###########################"
+echo "The uptime of the system is:"
+uptime
+echo
+
+# Memory Utilization
+echo "###########################"
+echo "Memory Utilization"
+free -m
+echo
+
+# Disk Utilization
+echo "###########################"
+echo "Disk Utilization"
+df -h
