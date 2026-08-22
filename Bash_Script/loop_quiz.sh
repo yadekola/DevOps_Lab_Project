cf8d7f917b820284f82601736f9ee634c0e493ec
yadekola <yakubadekolaojo059@gmail.com>
2026-08-22 13:27:45 +0000

    commit loop file

---
+++ Bash_Script/loop_quiz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#echo "Bash version ${BASH_VERSION}"
+#for i in {0..10..2}
+#     echo "Welcome $i times"
+# done
+
+
+#!/bin/bash
+#for (( c=1; c<=5; c++ ))
+#do
+#   echo "Welcome $c times"
+#done
+
+
+#!/bin/bash
+#Infinity loop
+
+for ((;;))
+do
+  echo $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM
+done
